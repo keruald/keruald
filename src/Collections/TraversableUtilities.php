a65de8f3d3b37c7d87645595d84599e0fac99ce3
Sébastien Santoro <dereckson@espace-win.org>
2018-05-15 20:59:57 +0000

    Support other countable legacy types
    Summary:
    Per https://github.com/Ayesh/is_countable-polyfill,
    SimpleXMLElement and ResourceBundle are countable too.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D1640

--- src/Collections/TraversableUtilities.php
+++ src/Collections/TraversableUtilities.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 namespace Keruald\OmniTools\Collections;
 
 use Countable;
+use ResourceBundle;
+use SimpleXMLElement;
 use TypeError;
 
 class TraversableUtilities {
 
     public static function count ($countable) : int {
-        if (is_array($countable)) {
+        if (self::isCountable($countable)) {
             return count($countable);
-        }
-
-        if ($countable instanceof Countable) {
-            return $countable->count();
         }
 
         if ($countable === null || $countable === false) {
@@ -22,6 +20,14 @@ class TraversableUtilities {
         }
 
         throw new TypeError;
+    }
+
+    public static function isCountable ($countable) : bool {
+        // https://github.com/Ayesh/is_countable-polyfill/blob/master/src/is_countable.php
+        return is_array($countable)
+               || $countable instanceof Countable
+               || $countable instanceof SimpleXMLElement
+               || $countable instanceof ResourceBundle;
     }
 
 }
