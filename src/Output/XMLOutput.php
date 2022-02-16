463ed23cb7f9471467294ad23690ab3dd5e54209
DorianWinty <dorianvanlaecke@hotmail.fr>
2022-02-16 23:32:36 +0100

    Output report in XML
    Summary: This change adds test and XML support.
    
    Test Plan:
    Unit tests for a regular report are provided.
    
    Only missing some cases if the rapport, or a part of it like properties, is empty.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Subscribers: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D2527

---
+++ src/Output/XMLOutput.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace Keruald\Reporting\Output;
+
+
+class XMLOutput extends Output {
+
+    public function render () : string {
+        $report = $this->report;
+        $document = xmlwriter_open_memory();
+
+        xmlwriter_set_indent($document, true);
+        xmlwriter_set_indent_string($document, '    ');
+
+        xmlwriter_start_document($document, '1.0', 'UTF-8');
+
+        xmlwriter_start_element($document, 'report');
+        xmlwriter_start_attribute($document, 'title');
+        xmlwriter_text($document, $report->title);
+        xmlwriter_end_attribute($document);
+
+        foreach ($report->sections as $section) {
+            xmlwriter_start_element($document, 'section');
+            xmlwriter_start_attribute($document, 'title');
+            xmlwriter_text($document, $section->title);
+            xmlwriter_end_attribute($document);
+
+            foreach ($section->entries as $entry) {
+                xmlwriter_start_element($document, 'entry');
+
+                xmlwriter_start_attribute($document, 'title');
+                xmlwriter_text($document, $entry->title);
+                xmlwriter_end_attribute($document);
+
+                xmlwriter_start_element($document, 'text');
+                xmlwriter_text($document, $entry->text);
+                xmlwriter_end_element($document);
+
+                xmlwriter_end_element($document);
+            }
+            xmlwriter_end_element($document); // section
+        }
+
+        xmlwriter_start_element($document, 'data');
+        xmlwriter_start_attribute($document, 'title');
+        xmlwriter_text($document, "Properties");
+        xmlwriter_end_attribute($document);
+
+        foreach ($report->properties as $key => $value) {
+            xmlwriter_start_element($document, 'entry');
+
+            xmlwriter_start_element($document, 'key');
+            xmlwriter_text($document, $key);
+            xmlwriter_end_element($document);
+
+            xmlwriter_start_element($document, 'value');
+            xmlwriter_text($document, $value);
+            xmlwriter_end_element($document);
+
+            xmlwriter_end_element($document);
+        }
+        xmlwriter_end_element($document); // data
+
+        xmlwriter_end_element($document); // report
+        xmlwriter_end_document($document);
+
+        return xmlwriter_output_memory($document);
+    }
+
+}
