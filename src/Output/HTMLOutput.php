1d8d4a822974629a49452a9b3ca235478924a6d3
DorianWinty <dorianvanlaecke@hotmail.fr>
2022-02-17 19:28:04 +0100

    Output report in HTML and Markdown
    Summary:
    **HTML implementation notes**
    
    HTML is more useful as a fragment than as full page,
    to be able to be included in a regular application.
    
    An entry can be divided as several paragraphs,
    as it could represent several ideas.
    
    Ref T1678
    
    Test Plan:
    Unit tests for a regular report are provided.
    
    Only missing some cases if the rapport, or a part of it like properties, is empty.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Subscribers: dereckson
    
    Maniphest Tasks: T1678
    
    Differential Revision: https://devcentral.nasqueron.org/D2505

--- src/Output/HTMLOutput.php
+++ src/Output/HTMLOutput.php
@@ -4,8 +4,61 @@ namespace Keruald\Reporting\Output;
 
 class HTMLOutput extends Output {
 
-    public function render () : string {
-        return "";
+    private function makeId ($name) : string {
+        return urlencode(strtolower(str_replace(' ', '-', $name)));
     }
 
+    private static function encode (string $text) : string {
+        return htmlspecialchars($text);
+    }
+
+    public function render () : string {
+
+        $send = [];
+        $title = $this->report->title;
+        $send[] =
+            '<h1 id="' . $this->makeId($title) . '">' . self::encode($title)
+            . '</h1>';
+
+        foreach ($this->report->sections as $section) {
+            $title = $section->title;
+            $send[] =
+                '<h2 id="' . $this->makeId($title) . '">' .
+                self::encode($title) . '</h2>';
+            foreach ($section->entries as $entry) {
+                $title = $entry->title;
+                $send[] = '<h3 id="' . $this->makeId($title) . '">' .
+                          self::encode($title) . '</h3>';
+
+                $text = explode("\n\n", $entry->text);
+
+                foreach ($text as $value) {
+                    $send[] = '<p>' . self::encode($value) . '</p>';
+                }
+            }
+        }
+        $send[] = '<hr>';
+
+        $send[] = '<h2 id="report-properties">Report properties</h2>';
+        $send[] = '<table>';
+        $send[] = str_repeat(" ", 4) . '<tbody>';
+
+        $properties = $this->report->properties;
+
+        foreach ($properties as $key => $value) {
+            $send[] = str_repeat(" ", 4) . '<tr>';
+            $send[] = str_repeat(" ", 8) .
+
+                      '<th>' . self::encode($key) . '</th>';
+            $send[] = str_repeat(" ", 8) .
+                      '<td>' . self::encode($value) . '</td>';
+
+            $send[] = str_repeat(" ", 4) . '</tr>';
+        }
+        $send[] = str_repeat(" ", 4) . '</tbody>';
+        $send[] = '</table>';
+        $send[] = '';
+
+        return implode("\n", $send);
+    }
 }
