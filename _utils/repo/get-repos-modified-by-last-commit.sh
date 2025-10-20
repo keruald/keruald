a51ef2e9abd3cf9b6d479655e3272f4c1fbdb169
Sébastien Santoro <dereckson@espace-win.org>
2025-10-20 23:23:20 +0000

    Improve detection of modified packages
    Summary:
    Ignore root files like composer.json: awk splits fields with path separator
    and only keep lines with more than one field, so at least a directory.
    
    Consolidate all operations in an awk script.
    
    Consolidate sort and uniq using `sort -u`.
    
    Test Plan: ./_utils/repo/get-repos-modified-by-last-commit.sh <several hashes>
    
    Differential Revision: https://devcentral.nasqueron.org/D3786

--- _utils/repo/get-repos-modified-by-last-commit.sh
+++ _utils/repo/get-repos-modified-by-last-commit.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-git diff-tree --no-commit-id --name-only -r HEAD | grep -v -e '_.*' | cut -f1 -d"/" | sort | uniq
+git diff-tree --no-commit-id --name-only -r HEAD \
+    | awk -F/ '$1 !~ /^_/ && NF > 1 { print $1 }' \
+    | sort -u
